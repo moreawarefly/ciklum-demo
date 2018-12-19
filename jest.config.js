62c83862799769ab8720b183ad132d52fdedbd9a
Marcin Skawiński <marcin@moreawarefly.com>
2018-12-19 01:51:08 +0100

    AddOrderForm test stub

--- jest.config.js
+++ jest.config.js
@@ -119,7 +119,10 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["jest-localstorage-mock"],
+  setupFiles: [
+    "jest-localstorage-mock",
+    "./tests/setupJest.js"
+  ],
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
   // setupTestFrameworkScriptFile: null,
